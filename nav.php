50790d0ac03adc392a8475db622ce992f655dc3f
Tom <tom-vdl@outlook.com>
2024-01-22 16:30:37 +0100

    pokemon project
    this is the main project

---
+++ nav.php
@@ -0,0 +1,10 @@
+<nav class="bar">
+  <div class="dropdown">
+    <button class="dropbtn DropbtnLogo" style="background-image: url('img/logo.png'); background-position: center;"></button>
+    <div class="dropdown-content">
+      <a href="index.php">Home</a>
+      <a href="#">Trainers</a>
+      <a href="#">Pokemon</a>
+    </div>
+  </div>
+</nav>
